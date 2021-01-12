e50b2c647483f4973636ac35a475b8eb14030eba
Kawal Deol <kawalpreetdeol@hotmail.com>
2021-01-12 16:12:21 -0500

    inital commit

---
+++ src/views/Experience.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="experience">
+    <h1>Experience Page Coming Soon to you ...</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Experience",
+  components: {}
+};
+</script>
